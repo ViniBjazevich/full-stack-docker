fac69dca4f10d3a9cf84f523badd8a4b38623c37
Vini Bjazevich <vinibjazevich@Vinis-iMac.local>
2022-08-11 12:32:11 -0500

    feat: created docker compose file

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,20 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/vini", (req, res) => {
+  res.send("Vini");
+});
+
+app.get("/bailey", (req, res) => {
+  res.send("bailey");
+});
+
+app.get("/users", (req, res) => {
+  axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(({data}) => res.send(data))
+    .catch((err) => res.send(e))
 });
 
 app.listen(port, () => {
